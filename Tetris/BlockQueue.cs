d066bc956506d4091cd6162d0c5d01accee80aa9
Megan Rakow <mrakow@rcmowersusa.com>
2023-03-13 10:44:00 -0500

    Finished game

--- Tetris/BlockQueue.cs
+++ Tetris/BlockQueue.cs
@@ -1,12 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using System;
 
 namespace Tetris
 {
-    class BlockQueue
+    public class BlockQueue
     {
+        private readonly Block[] blocks = new Block[]
+        {
+            new IBlock(),
+            new JBlock(),
+            new ZBlock(),
+            new OBlock(),
+            new LBlock(),
+            new SBlock(),
+            new TBlock()
+        };
+
+        private readonly Random random = new Random();
+
+        public Block NextBlock { get; private set; }
+
+        public BlockQueue()
+        {
+            NextBlock= RandomBlock();
+        }
+
+        private Block RandomBlock()
+        {
+            return blocks[random.Next(blocks.Length)];
+        }
+
+        public Block GetAndUpdtae()
+        {
+            Block block = NextBlock;
+            do 
+            {
+                NextBlock = RandomBlock();
+            }
+            while (block.Id == NextBlock.Id);
+
+            return block;
+        }
     }
 }
--- Tetris/GameState.cs
+++ Tetris/GameState.cs
@@ -17,18 +17,51 @@ namespace Tetris
             {
                 currentBlock = value;
                 currentBlock.Reset();
+
+                for (int i = 0; i < 2; i++)
+                {
+                    currentBlock.Move(1, 0);
+                    if (!BlockFits())
+                    {
+                        currentBlock.Move(-1, 0);
+                    }
+                }
             }
         }
 
         public GameGrid GameGrid { get; }
         public BlockQueue BlockQueue { get; }
         public bool GameOver { get; private set; }
-
+        public int Score { get; private set; }
+        public Block HeldBlock { get; private set; }
+        public bool CanHold { get; private set; }
         public GameState() 
         {
             GameGrid = new GameGrid(22, 10);
             BlockQueue= new BlockQueue();
             CurrentBlock = BlockQueue.GetAndUpdtae();
+            CanHold = true;
+        }
+
+        public void HoldBlock() 
+        {
+            if (!CanHold)
+            {
+                return;
+            }
+
+            if (HeldBlock == null)
+            {
+                HeldBlock = CurrentBlock;
+                CurrentBlock = BlockQueue.GetAndUpdtae();
+            }
+            else 
+            {
+                Block tmp = CurrentBlock;
+                CurrentBlock = HeldBlock;
+                HeldBlock = tmp;
+            }
+            CanHold= false;
         }
 
         private bool BlockFits()
@@ -92,7 +125,7 @@ namespace Tetris
                 GameGrid[p.Row, p.Column] = CurrentBlock.Id;
             }
 
-            GameGrid.ClearFullRows();
+            Score =+ GameGrid.ClearFullRows();
 
             if (IsGameOver())
             {
@@ -101,6 +134,7 @@ namespace Tetris
             else
             {
                 CurrentBlock = BlockQueue.GetAndUpdtae();
+                CanHold = true;
             }
         }
 
@@ -113,6 +147,35 @@ namespace Tetris
                 CurrentBlock.Move(-1, 0);
                 PlaceBlock();
             }
+        }
+
+        private int TileDropDistance(Position p)
+        {
+            int drop = 0;
+
+            while (GameGrid.IsEmpty(p.Row + drop + 1, p.Column))
+            {
+                drop++;
+            }
+            return drop;
+        }
+
+        public int BlockDropDistance()
+        {
+            int drop = GameGrid.Rows;
+
+            foreach (Position p in CurrentBlock.TilePositions())
+            {
+                drop = System.Math.Min(drop, TileDropDistance(p));
+            }
+
+            return drop;
+        }
+
+        public void DropBlock()
+        {
+            CurrentBlock.Move(BlockDropDistance(), 0);
+            PlaceBlock();
         }
     }
 }
